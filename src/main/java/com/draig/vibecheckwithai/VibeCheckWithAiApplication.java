d657fefe5570753c0eac2d427d8caf379234e60d
grv <garv.sharma200212@gmail.com>
2025-09-28 15:21:49 +0530

    Initial commit of the project

---
+++ src/main/java/com/draig/vibecheckwithai/VibeCheckWithAiApplication.java
@@ -0,0 +1,13 @@
+package com.draig.vibecheckwithai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VibeCheckWithAiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VibeCheckWithAiApplication.class, args);
+    }
+
+}
